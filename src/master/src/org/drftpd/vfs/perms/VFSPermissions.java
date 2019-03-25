c7237ebc853f037aca1bc2ca971f062fa0920798
Epochyx <epochyx@tutanota.com>
2019-03-25 21:42:46 +0100

    Formatting

--- src/master/src/org/drftpd/vfs/perms/VFSPermissions.java
+++ src/master/src/org/drftpd/vfs/perms/VFSPermissions.java
@@ -30,22 +30,23 @@ import java.util.Map.Entry;
 
 /**
  * This object handles all the permissions releated to the VFS.
+ * 
  * @author fr0w
  * @version $Id$
  */
 public class VFSPermissions {
 	private final static Logger logger = Logger.getLogger(VFSPermissions.class);
 
-	private HashMap<String, PermissionWrapper> _handlersMap;	
-	
-	// HashMap<Type, HashMap<Directive, List<PathPermission>>> 
-	private HashMap<String, HashMap<String, LinkedList<PathPermission>>> _pathPerms;	
+	private HashMap<String, PermissionWrapper> _handlersMap;
+
+	// HashMap<Type, HashMap<Directive, List<PathPermission>>>
+	private HashMap<String, HashMap<String, LinkedList<PathPermission>>> _pathPerms;
 	private HashMap<String, String> _directiveToType;
 	private HashMap<String, TreeMap<Integer, String>> _priorities;
-	
+
 	public VFSPermissions() {
 		loadExtensions();
-		
+
 		_pathPerms = new HashMap<>();
 	}
 
@@ -55,44 +56,39 @@ public class VFSPermissions {
 		_priorities = new HashMap<>();
 
 		/*
-		<extension-point id="VFSPerm">
-	      	<parameter-def id="Class" />
-	    	<parameter-def id="Method" />
-	    	<parameter-def id="Type" />
-	     	<parameter-def id="Directive" />
-	     	<parameter-def id="Priority" />
-	   	</extension-point>
-		*/
+		 * <extension-point id="VFSPerm"> <parameter-def id="Class" /> <parameter-def
+		 * id="Method" /> <parameter-def id="Type" /> <parameter-def id="Directive" />
+		 * <parameter-def id="Priority" /> </extension-point>
+		 */
 
 		try {
-			List<PluginObjectContainer<VFSPermHandler>> loadedHandlers =
-				CommonPluginUtils.getPluginObjectsInContainer(this, "master", "VFSPerm", "Class", "Method",
-						new Class[] { String.class, StringTokenizer.class });
+			List<PluginObjectContainer<VFSPermHandler>> loadedHandlers = CommonPluginUtils.getPluginObjectsInContainer(
+					this, "master", "VFSPerm", "Class", "Method", new Class[] { String.class, StringTokenizer.class });
 			for (PluginObjectContainer<VFSPermHandler> container : loadedHandlers) {
 				String directive = container.getPluginExtension().getParameter("Directive").valueAsString();
 
 				if (_handlersMap.containsKey(directive)) {
-					logger.debug("A handler for '"+ directive +"' already loaded, check your plugin.xml's");
+					logger.debug("A handler for '" + directive + "' already loaded, check your plugin.xml's");
 					continue;
 				}
 
 				String type = container.getPluginExtension().getParameter("Type").valueAsString().toLowerCase();
 
 				if (!verifyType(type)) {
-					logger.debug("Invalid VFS permission type ("+type+") for directive '"+directive+"'.");
+					logger.debug("Invalid VFS permission type (" + type + ") for directive '" + directive + "'.");
 					continue;
 				}
-				PermissionWrapper pw = new PermissionWrapper(container.getPluginObject(), container.getPluginMethod());				
+				PermissionWrapper pw = new PermissionWrapper(container.getPluginObject(), container.getPluginMethod());
 				_handlersMap.put(directive, pw);
 				_directiveToType.put(directive, type);
-				
+
 				// building execution order.
 				int priority = container.getPluginExtension().getParameter("Priority").valueAsNumber().intValue();
-                TreeMap<Integer, String> order = _priorities.computeIfAbsent(type, k -> new TreeMap<>());
-                while (true) {
+				TreeMap<Integer, String> order = _priorities.computeIfAbsent(type, k -> new TreeMap<>());
+				while (true) {
 					if (order.containsKey(priority)) {
-						logger.debug("The slot that " + directive + " is trying to use is already allocated, " +
-								"check the xmls, allocating the next available slot");
+						logger.debug("The slot that " + directive + " is trying to use is already allocated, "
+								+ "check the xmls, allocating the next available slot");
 						priority++;
 					} else {
 						order.put(priority, directive);
@@ -102,44 +98,44 @@ public class VFSPermissions {
 			}
 		} catch (IllegalArgumentException e) {
 			logger.error("Failed to load plugins for master extension point 'VFSPerm', possibly the master"
-					+" extension point definition has changed in the plugin.xml",e);
+					+ " extension point definition has changed in the plugin.xml", e);
 		}
 	}
-	
+
 	private boolean verifyType(String type) {
 		type = type.toLowerCase();
-        return type.equals("upload") || type.equals("makedir") || type.equals("delete")
-                || type.equals("deleteown") || type.startsWith("rename") || type.equals("renameown")
-                || type.equals("privpath") || type.equals("download");
+		return type.equals("upload") || type.equals("makedir") || type.equals("delete") || type.equals("deleteown")
+				|| type.startsWith("rename") || type.equals("renameown") || type.equals("privpath")
+				|| type.equals("download");
 
-    }
-	
+	}
+
 	public void handleLine(String directive, StringTokenizer st) {
 		if (!_handlersMap.containsKey(directive)) {
-			throw new UnsupportedOperationException("No VFSPermHandler found for this directive: "+ directive);
+			throw new UnsupportedOperationException("No VFSPermHandler found for this directive: " + directive);
 		}
-		
+
 		PermissionWrapper pw = _handlersMap.get(directive);
-		
+
 		pw.handle(directive, st);
 	}
-	
+
 	protected void addPermissionToMap(String directive, PathPermission pathPerm) {
 		String type = _directiveToType.get(directive);
 
-        HashMap<String, LinkedList<PathPermission>> map = _pathPerms.computeIfAbsent(type, k -> new HashMap<>());
+		HashMap<String, LinkedList<PathPermission>> map = _pathPerms.computeIfAbsent(type, k -> new HashMap<>());
 
-        LinkedList<PathPermission> list;
+		LinkedList<PathPermission> list;
 		if (!map.containsKey(directive)) {
 			list = new LinkedList<>();
 			map.put(directive, list);
 		} else {
 			list = map.get(directive);
 		}
-		
+
 		list.add(pathPerm);
 	}
-	
+
 	public boolean checkPathPermission(String type, User user, InodeHandle inode) {
 		return checkPathPermission(type, user, inode, false);
 	}
@@ -147,36 +143,38 @@ public class VFSPermissions {
 	public boolean checkPathPermission(String type, User user, InodeHandle inode, boolean defaults) {
 		return checkPathPermission(type, user, inode, defaults, false);
 	}
-	
-	public boolean checkPathPermission(String type, User user, InodeHandle inode, boolean defaults, boolean invertUserSemantic) {
-		
+
+	public boolean checkPathPermission(String type, User user, InodeHandle inode, boolean defaults,
+			boolean invertUserSemantic) {
+
 		if (!verifyType(type)) {
 			throw new IllegalArgumentException("Invalid VFS perm type.");
 		}
-		
+
 		HashMap<String, LinkedList<PathPermission>> map = _pathPerms.get(type);
 		TreeMap<Integer, String> order = _priorities.get(type);
-		
+
 		if (map == null) {
 			return defaults;
 		}
-		
+
 		if (order == null) {
-			NullPointerException npe = new NullPointerException("You've got some screwy plugin.xml files!  Blame fr0w!");
+			NullPointerException npe = new NullPointerException(
+					"You've got some screwy plugin.xml files!  Blame fr0w!");
 			logger.error(npe, npe);
 			throw npe;
 		}
-				
+
 		for (Entry<Integer, String> entry : order.entrySet()) {
 			String directive = entry.getValue();
-			
+
 			LinkedList<PathPermission> perms = map.get(directive);
-			
+
 			if (perms == null) {
 				// 'directive' was not found in perms.conf
 				continue;
 			}
-			
+
 			if (!perms.isEmpty()) {
 				for (PathPermission perm : perms) {
 					if (perm.checkPath(inode)) {
@@ -188,7 +186,7 @@ public class VFSPermissions {
 				}
 			}
 		}
-		
+
 		return defaults;
 	}
 
@@ -210,14 +208,14 @@ public class VFSPermissions {
 		sb.append('(');
 
 		for (PathPermission perm : perms) {
-			// Make a regex	based on user perms, assume no permission if user=null
+			// Make a regex based on user perms, assume no permission if user=null
 			if (user == null || !perm.check(user)) {
 				// User do not have permission
 				if (sb.length() != 1) {
 					// not first perm to add, add a | prefix
 					sb.append('|');
 				}
-				sb.append(((GlobPathPermission)perm).getPattern().pattern());
+				sb.append(((GlobPathPermission) perm).getPattern().pattern());
 			}
 		}
 
@@ -229,24 +227,24 @@ public class VFSPermissions {
 		}
 		return sb.toString();
 	}
-	
+
 	// if you want to debug this class, call this method.
 	public void dumpMap() {
 		for (Entry<String, HashMap<String, LinkedList<PathPermission>>> e1 : _pathPerms.entrySet()) {
 			String type = e1.getKey();
 			HashMap<String, LinkedList<PathPermission>> map = e1.getValue();
-			
+
 			logger.debug(type + " is handling:");
 			TreeMap<Integer, String> order = _priorities.get(type);
-            for (Entry<Integer, String> e2 : order.entrySet()) {
-                String directive = e2.getValue();
-                logger.debug(e2.getKey() + ". " + directive);
-                if (map.get(directive) == null) {
-                    // 'directive' was not found in perms.conf
-                    continue;
-                }
-                logger.debug(map.get(directive).toString());
-            }
+			for (Entry<Integer, String> e2 : order.entrySet()) {
+				String directive = e2.getValue();
+				logger.debug(e2.getKey() + ". " + directive);
+				if (map.get(directive) == null) {
+					// 'directive' was not found in perms.conf
+					continue;
+				}
+				logger.debug(map.get(directive).toString());
+			}
 		}
 	}
 }
